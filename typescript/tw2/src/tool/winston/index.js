53af129aca0d7404d09f67fd061704ea5b18a768
Xostron <aat9044107480@gmail.com>
2024-12-26 17:24:30 +0300

    fix

--- typescript/tw2/src/tool/winston/index.js
+++ typescript/tw2/src/tool/winston/index.js
@@ -8,43 +8,43 @@ const { combine, timestamp, json } = format
 const custom = { levels: { error: 1, user: 2, state: 3, work: 4, all: 5 } }
 // Фильтры
 function filterLog(type) {
-	return format((info, opts) => (info.level === type ? info : false))
+	return format((info, opts) => (info.level === type ? info : false))()
 }
 
 // Шаблоны файлов логов
 templates = [
 	{
 		filename: './data/error-%DATE%.log',
-		datePattern: 'YYYY-MM-DD hh-mm',
-		maxFiles: '1m',
+		datePattern: 'YYYY-MM-DD-HH',
+		maxFiles: '1h',
 		level: 'error',
-		format: combine(filterLog('error')()),
+		format: combine(filterLog('error')),
 	},
 	{
 		filename: './data/user-%DATE%.log',
-		datePattern: 'YYYY-MM-DD hh-mm',
-		maxFiles: '1m',
+		datePattern: 'YYYY-MM-DD-HH',
+		maxFiles: '1h',
 		level: 'user',
-		format: combine(filterLog('user')()),
+		format: combine(filterLog('user')),
 	},
 	{
 		filename: './data/state-%DATE%.log',
-		datePattern: 'YYYY-MM-DD hh-mm',
-		maxFiles: '1m',
+		datePattern: 'YYYY-MM-DD-HH',
+		maxFiles: '1h',
 		level: 'state',
-		format: combine(filterLog('state')()),
+		format: combine(filterLog('state')),
 	},
 	{
 		filename: './data/work-%DATE%.log',
-		datePattern: 'YYYY-MM-DD hh-mm',
-		maxFiles: '1m',
+		datePattern: 'YYYY-MM-DD-HH',
+		maxFiles: '1h',
 		level: 'work',
-		format: combine(filterLog('work')()),
+		format: combine(filterLog('work')),
 	},
 	{
 		filename: './data/all-%DATE%.log',
-		datePattern: 'YYYY-MM-DD hh-mm',
-		maxFiles: '1m',
+		datePattern: 'YYYY-MM-DD-HH',
+		maxFiles: '1h',
 		level: 'all',
 	},
 ]

85c9d323153efb5272a3b9e4ed54faa735c19078
Xostron <aat9044107480@gmail.com>
2025-01-14 13:01:17 +0300

    fix

---
+++ typescript/tw4-react/src/page/item/index.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent, FC, useState } from 'react'
+import { Prd } from '@tool/product'
+
+type Props = {
+	prd: Prd
+	cb: (prd: Prd, count: number) => void
+}
+
+export const PrdItem: FC<Props> = (props) => {
+	const { prd, cb } = props
+	const [count, setCount] = useState<number>(1)
+
+	return (
+		<article>
+			<h4>
+				{prd.name}
+				<span>${prd.price.toFixed(2)}</span>
+			</h4>
+			<div>
+				{prd.desc}
+				<button onClick={() => cb(prd, count)}>+</button>
+			</div>
+			<select name='' id='' onChange={(e) => setCount(Number(e.target.value))}>
+				<option value='1'></option>
+				<option value='2'></option>
+				<option value='3'></option>
+			</select>
+		</article>
+	)
+}

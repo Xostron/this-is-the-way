b6fd7b5f0cbf048a0899af7d191409bc648f58b4
Xostron <aat9044107480@gmail.com>
2025-01-14 11:28:54 +0300

    fix Product

--- typescript/tw4-react/src/tool/primarch/index.ts
+++ typescript/tw4-react/src/tool/primarch/index.ts
@@ -1,15 +1,39 @@
-export type Card = {
+export type Prd = {
 	id: number
 	name: string
-	descr: string
-	health: number
-	damage: string
+	desc: string
+	category: string
+	price: number
 }
 
-export type CardChoice = {
-	card:Card, owner:string
+export type PrdSel = {
+	product: Prd
+	count: number
 }
 
-export class CardChoicePower {
-	// public static total()
+export class PrdSelHelp {
+	// Стоимость всех продуктов
+	public static total(selections: PrdSel[]) {
+		return selections.reduce((acc, item, i) => acc + item.product.price * item.count, 0)
+	}
+	// Количество продуктов
+	public static productCount(selections: PrdSel[]) {
+		return selections.reduce((acc, item, i) => acc + item.count, 0)
+	}
 }
+
+export class PrdSelMut {
+	// Добавить продукт
+	public static addPrd(selections: PrdSel[], prd: Prd, count: number) {}
+	// Убрать продукт
+	public static removePrd(selections: PrdSel[], id: number) {
+		selections.forEach((el, i) => {
+			if (el.product.id === i) {
+				selections = selections.splice(i, 1)
+			}
+		})
+	}
+}
+
+// export type { Prd, PrdSel }
+// export { PrdSelHelp, PrdSelMut }

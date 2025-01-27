b23ba22c985dcf94cefbfc01982339d15ad6f804
Xostron <aat9044107480@gmail.com>
2025-01-27 16:35:57 +0300

    css

--- front/src/page/main/index.jsx
+++ front/src/page/main/index.jsx
@@ -1,13 +1,93 @@
-import Nav from "@cmp/nav"
-import Footer from "@cmp/footer"
-import List from "./list"
-import "./style.css"
+import Nav from '@cmp/nav'
+import Footer from '@cmp/footer'
+import List from './list'
+import './style.css'
+
+const Main = () => {
+	console.time('all')
+	all(obj, data)
+	console.timeEnd('all')
+
+	console.log(111, obj)
+
+	console.time('get')
+	const r = getKeys(obj)
+	console.timeEnd('get')
+
+	console.log(222, r)
+
+	return (
+		<main className='page'>
+			<section className='header'>1</section>
+			<section className='content'>
+				<h1>2</h1>
+				<List />
+				<section className='list-card'>
+					{new Array(20).fill(0).map((el, i) => (
+						<article className='card' key={i}>
+							<h1>Title {i}</h1>
+							<p className='card-text'>asdas asdasd</p>
+							<button className='card-btn'>Кнопка</button>
+						</article>
+					))}
+				</section>
+			</section>
+			<section className='asidel'>3</section>
+			<section className='asider'>4</section>
+			<section className='footer'>5</section>
+		</main>
+	)
+}
+
+export default Main
+
+// (рекурсия) Получить ключи объекта
+function getKeys(o) {
+	const keys = Object.keys(o)
+	const r = []
+	keys.forEach((k) => {
+		if (typeof o[k] !== 'object') return
+		const rr = getKeys(o[k])?.filter((el) => el)
+		const t = { key: k }
+		rr ? (t.nest = rr) : null
+		r.push(t)
+	})
+	return r.length ? r : null
+}
+
+/**
+ * obj - актуальные аварии, data - сохраненые аварии
+ * Поиск пересечения между двумя объектами, результат мутированный obj,
+ * в котором пересечения копируются из data
+ * @param {*} data данные из файла (слудующая вложенность)
+ * @param {*} obj данные из итерации (следующая вложенность)
+ * @param {*} prev данные из итерации (предыдущая вложенность)
+ * @param {*} key предыдущий ключ
+ * @returns
+ */
+function all(obj, data, prev, key) {
+	const keys = Object.keys(obj)
+	// console.log(111, key, obj, keys)
+	for (const k of keys) {
+		// Ключ есть в файле (пересечение obj c data)
+		if (k in data) {
+			if (typeof obj[k] !== 'object') {
+				// console.log(555, key, prev, data)
+				prev[key] = data
+				return
+			}
+			// console.log(444, k, obj)
+			all(obj[k], data[k], obj, k)
+		}
+	}
+}
+
 const data = {
 	// полное совпадение
 	bld1: {
 		sec11: { low1: { date: 11, id: 21, code: 31 } },
 		sec12: { low2: { date: 12, id: 22, code: 32 } },
-		sec13: { low3: {on:{ date: 13, id: 23, code: 33 }} },
+		sec13: { low3: { on: { date: 13, id: 23, code: 33 } } },
 	},
 	// частичное
 	bld2: {
@@ -58,7 +138,7 @@ const obj = {
 		sec11: { low1: { date: 10, id: 20, code: 30 } },
 		sec12: { low2: { date: 10, id: 20, code: 30 } },
 		sec13: {
-			low3:{on:{ date: 1, id: 2, code: 3 }},
+			low3: { on: { date: 1, id: 2, code: 3 } },
 			low4: { date: 10, id: 20, code: 30 },
 		},
 	},
@@ -103,72 +183,4 @@ const obj = {
 		// sec12: { low2: { date: 10, id: 20, code: 30 } },
 		// sec12: { low3: { date: 10, id: 20, code: 30 } },
 	},
-}
-const Main = () => {
-	console.time('all')
-	all(obj, data)
-	console.timeEnd('all')
-
-	console.log(111, obj)
-	
-	console.time('get')
-	const r = getKeys(obj)
-	console.timeEnd('get')
-	
-	console.log(222, r)
-	// console.log("main")
-	return (
-		<main className='page'>
-			<div className='header'>1</div>
-			<div className='content'>
-				<List />
-			</div>
-			<div className='asidel'>3</div>
-			<div className='asider'>4</div>
-			<div className='footer'>5</div>
-		</main>
-	)
-}
-
-export default Main
-
-// (рекурсия) Получить ключи объекта
-function getKeys(o) {
-	const keys = Object.keys(o)
-	const r = []
-	keys.forEach((k) => {
-		if (typeof o[k] !== "object") return
-		const rr = getKeys(o[k])?.filter((el) => el)
-		const t = { key: k }
-		rr ? (t.nest = rr) : null
-		r.push(t)
-	})
-	return r.length ? r : null
-}
-
-/**
- * obj - актуальные аварии, data - сохраненые аварии
- * Поиск пересечения между двумя объектами, результат мутированный obj,
- * в котором пересечения копируются из data
- * @param {*} data данные из файла (слудующая вложенность)
- * @param {*} obj данные из итерации (следующая вложенность)
- * @param {*} prev данные из итерации (предыдущая вложенность)
- * @param {*} key предыдущий ключ
- * @returns
- */
-function all(obj, data, prev, key) {
-	const keys = Object.keys(obj)
-	// console.log(111, key, obj, keys)
-	for (const k of keys) {
-		// Ключ есть в файле (пересечение obj c data)
-		if (k in data) {
-			if (typeof obj[k] !== "object") {
-				// console.log(555, key, prev, data)
-				prev[key] = data
-				return
-			}
-			// console.log(444, k, obj)
-			all(obj[k], data[k], obj, k)
-		}
-	}
 }

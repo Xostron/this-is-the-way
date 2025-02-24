2f463401bfd5f5da6dcdce7e8ba6208f4cc38e61
Xostron <aat9044107480@gmail.com>
2025-02-24 20:52:05 +0300

    fix

--- front/src/cmp/d3/bar/index.jsx
+++ front/src/cmp/d3/bar/index.jsx
@@ -1,20 +1,12 @@
-import * as d3 from 'd3'
-import { useEffect, useRef } from 'react'
-import './style.css'
+import * as d3 from "d3"
+import { useEffect, useRef } from "react"
+import "./style.css"
 
-const Bar = () => {
+const Bar = ({ width = 500, height = 300, data }) => {
 	const svgRef = useRef()
 
-	const data = [
-		{ name: '20.02.2024', value: 10 },
-		{ name: '21.02.2024', value: 7 },
-		{ name: '22.02.2024', value: 15 },
-	]
-
 	// Размеры
-	const width = 600
-	const height = 400
-	const margin = { top: 20, right: 20, bottom: 30, left: 40 }
+	const margin = { top: 30, right: 20, bottom: 40, left: 40 }
 	const innerW = width - margin.left - margin.right
 	const innerH = height - margin.top - margin.bottom
 
@@ -23,63 +15,85 @@ const Bar = () => {
 
 	useEffect(() => {
 		// Очистка предыдущего графика
-		d3.select(svgRef.current).selectAll('*').remove()
+		d3.select(svgRef.current).selectAll("*").remove()
 		// svg - контейнер
-		const svg = d3.select(svgRef.current).attr('width', width).attr('height', height).append('g')
+		const svg = d3
+			.select(svgRef.current)
+			.attr("width", width)
+			.attr("height", height)
+			.append("g")
 
-		// Шкалы
+		// Шкала X
 		const xScale = d3
 			.scaleBand()
 			.domain(data.map((d) => d.name))
 			.range([0, innerW])
-			.padding(0.1)
-
+			.padding(1)
+		// Шкала Y
 		const yScale = d3
 			.scaleLinear()
 			.domain([0, d3.max(data, (d) => d.value)])
 			.range([innerH, 0])
 			.nice()
 
-		// Оси
-		svg.append('g')
-			.attr('transform', `translate(${margin.left},${20})`)
-			.call(d3.axisLeft(yScale))
-
-		svg.append('g')
-			.attr('transform', `translate(${margin.left},${innerH + 20})`)
+		// Ось X
+		svg.append("g")
+			.attr("transform", `translate(${margin.left},${innerH + 20})`)
 			.call(d3.axisBottom(xScale))
+		// Ось Y
+		const yAxis = d3.axisLeft(yScale).ticks(5) 
+		svg.append("g")
+			.attr("transform", `translate(${margin.left},${20})`)
+			.call(yAxis)
+			
+
+		// Подпись X
+		svg.append("text")
+			.attr(
+				"transform",
+				`translate(${innerW / 2 + margin.left},${
+					height - margin.bottom / 3
+				})`
+			)
+			.style("text-anchor", "middle")
+			.text("Значения")
+			.attr("fill", "white")
+		// Подпись Y
+		svg.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("x", -height / 2)
+			.attr("y", margin.left / 2)
+			.style("text-anchor", "middle")
+			.text("Частота")
+			.attr("fill", "white")
 
 		// График
-		svg.selectAll('rect')
+		svg.selectAll("rect")
 			.data(data)
 			.enter()
-			.append('rect')
-			.attr('x', (d, i) => xScale(d.name))
-			.attr('y', (d) => yScale(d.value))
-			.attr('transform', `translate(${90},${margin.top})`)
-			.attr('width', barWidth)
-			.attr('height', (d) => innerH - yScale(d.value))
-			.attr('fill', 'steelblue')
-
-		// // Подписи осей
-		svg.append('text')
-			.attr('transform', `translate(${width / 2},${height - 10})`)
-			.style('text-anchor', 'middle')
-			.text('Значения')
-			.attr('fill', 'white')
-
-		svg.append('text')
-			.attr('transform', 'rotate(-90)')
-			.attr('x', -height / 2)
-			.attr('y', 20)
-			.style('text-anchor', 'middle')
-			.text('Частота')
-			.attr('fill', 'white')
+			.append("rect")
+			.attr("x", (d, i) => xScale(d.name))
+			.attr("y", (d) => yScale(d.value))
+			.attr("width", (d) => barWidth)
+			.attr("height", (d) => innerH - yScale(d.value))
+			.attr(
+				"transform",
+				(d) =>
+					`translate(${margin.left - barWidth / 2},${
+						margin.bottom / 2
+					})`
+			)
+			.attr("fill", "steelblue")
 	}, [])
 
 	return (
 		<>
-			<svg className='svg-border' ref={svgRef} width={barWidth * data.length} height='150'></svg>
+			<svg
+				className='svg-border'
+				ref={svgRef}
+				// width={barWidth * data.length}
+				// height='150'
+			></svg>
 			<ul></ul>
 		</>
 	)
--- front/src/page/boost/index.jsx
+++ front/src/page/boost/index.jsx
@@ -3,14 +3,14 @@ import { useEffect, useRef } from 'react'
 import World from '../../cmp/d3/world'
 import Bar from '../../cmp/d3/bar'
 import Line from '../../cmp/d3/line'
-const data = [20, 100, 60, 40, 70]
+const data = [{name:'20.02.2025',value:10}, {name:'21.02.2025',value:5},{name:'22.02.2025',value:15}]
 
 const Boost = () => {
 	return (
 		<main className='page'>
 			<section className='content'>
 				{/* <World /> */}
-				<Bar/>
+				<Bar data={data}/>
 				{/* <Line /> */}
 			</section>
 		</main>

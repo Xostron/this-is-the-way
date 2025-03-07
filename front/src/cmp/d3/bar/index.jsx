edef1eca97689728369e29c73c7660a85c7c408c
Xostron <aat9044107480@gmail.com>
2025-03-07 16:29:27 +0300

    fix

--- front/src/cmp/d3/bar/index.jsx
+++ front/src/cmp/d3/bar/index.jsx
@@ -1,89 +1,76 @@
-import * as d3 from "d3"
-import { useEffect, useRef } from "react"
-import "./style.css"
+import * as d3 from 'd3'
+import { useEffect, useRef } from 'react'
+import './style.css'
 
+// Гистограмма
 const Bar = ({ width = 500, height = 300, data }) => {
 	const svgRef = useRef()
-
 	// Размеры
 	const margin = { top: 30, right: 20, bottom: 40, left: 40 }
 	const innerW = width - margin.left - margin.right
 	const innerH = height - margin.top - margin.bottom
-
-	// Ширина столбика гистограммы
-	const barWidth = 50
+	const paddingBar = 5
 
 	useEffect(() => {
 		// Очистка предыдущего графика
-		d3.select(svgRef.current).selectAll("*").remove()
+		d3.select(svgRef.current).selectAll('*').remove()
 		// svg - контейнер
-		const svg = d3
-			.select(svgRef.current)
-			.attr("width", width)
-			.attr("height", height)
-			.append("g")
+		const svg = d3.select(svgRef.current).attr('width', width).attr('height', height).append('g')
 
-		// Шкала X
+		// ************************************************************
+		// Шкала X - категории
 		const xScale = d3
 			.scaleBand()
 			.domain(data.map((d) => d.name))
 			.range([0, innerW])
 			.padding(1)
-		// Шкала Y
+		// Ширина столбика гистограммы
+		const barWidth = data?.length ? data.map((el) => xScale(el.name))[0] - paddingBar : 0
+
+		// Шкала Y - значение
 		const yScale = d3
 			.scaleLinear()
 			.domain([0, d3.max(data, (d) => d.value)])
 			.range([innerH, 0])
 			.nice()
 
-		// Ось X
-		svg.append("g")
-			.attr("transform", `translate(${margin.left},${innerH + 20})`)
+		// ************************************************************
+		// Линия Ось X
+		svg.append('g')
+			.attr('transform', `translate(${margin.left},${innerH + 20})`)
 			.call(d3.axisBottom(xScale))
-		// Ось Y
-		const yAxis = d3.axisLeft(yScale).ticks(5) 
-		svg.append("g")
-			.attr("transform", `translate(${margin.left},${20})`)
-			.call(yAxis)
-			
+		// Линия Ось Y - 5 засечек
+		const yAxis = d3.axisLeft(yScale).ticks(5)
+		svg.append('g').attr('transform', `translate(${margin.left},${20})`).call(yAxis)
 
-		// Подпись X
-		svg.append("text")
-			.attr(
-				"transform",
-				`translate(${innerW / 2 + margin.left},${
-					height - margin.bottom / 3
-				})`
-			)
-			.style("text-anchor", "middle")
-			.text("Значения")
-			.attr("fill", "white")
-		// Подпись Y
-		svg.append("text")
-			.attr("transform", "rotate(-90)")
-			.attr("x", -height / 2)
-			.attr("y", margin.left / 2)
-			.style("text-anchor", "middle")
-			.text("Частота")
-			.attr("fill", "white")
+		// ************************************************************
+		// Подпись оси X
+		svg.append('text')
+			.attr('transform', `translate(${innerW / 2 + margin.left},${height - margin.bottom / 3})`)
+			.style('text-anchor', 'middle')
+			.text('Значения')
+			.attr('fill', 'white')
+		// Подпись  оси Y
+		svg.append('text')
+			.attr('transform', 'rotate(-90)')
+			.attr('x', -height / 2)
+			.attr('y', margin.left / 2.5)
+			.style('text-anchor', 'middle')
+			.text('Частота')
+			.attr('fill', 'white')
 
+		// ************************************************************
 		// График
-		svg.selectAll("rect")
+		svg.selectAll('rect')
 			.data(data)
 			.enter()
-			.append("rect")
-			.attr("x", (d, i) => xScale(d.name))
-			.attr("y", (d) => yScale(d.value))
-			.attr("width", (d) => barWidth)
-			.attr("height", (d) => innerH - yScale(d.value))
-			.attr(
-				"transform",
-				(d) =>
-					`translate(${margin.left - barWidth / 2},${
-						margin.bottom / 2
-					})`
-			)
-			.attr("fill", "steelblue")
+			.append('rect')
+			.attr('x', (d, i) => xScale(d.name))
+			.attr('y', (d) => yScale(d.value))
+			.attr('width', (d) => barWidth)
+			.attr('height', (d) => innerH - yScale(d.value))
+			.attr('transform', (d) => `translate(${margin.left - barWidth / 2},${margin.bottom / 2})`)
+			.attr('fill', 'steelblue')
 	}, [])
 
 	return (
--- front/src/page/boost/index.jsx
+++ front/src/page/boost/index.jsx
@@ -3,20 +3,16 @@ import { useEffect, useRef } from 'react'
 import World from '../../cmp/d3/world'
 import Bar from '../../cmp/d3/bar'
 import Line from '../../cmp/d3/line'
-const data = [
-	{ name: '20.02.2025', value: 10 },
-	{ name: '21.02.2025', value: 5 },
-	{ name: '22.02.2025', value: 15 },
-]
+
 
 const Boost = () => {
 	return (
 		<main className='page'>
 			<section className='content'>
 				{/* <World /> */}
-				<Bar data={data} />
-				{/* <Line /> */}
-				<p>{dewpoint()}</p>
+				<Bar data={dataBar} />
+				<Line data={dataLine}/>
+
 			</section>
 		</main>
 	)
@@ -24,16 +20,38 @@ const Boost = () => {
 
 export default Boost
 
-/**
- * Точка росы
- * https://poliol.ru/cont/articles/3-sposoba-rascheta-tochki-rosy-pri-uteplenii-pomeshcheniya/
- * @param {number} [h=50] Температура, С
- * @param {number} [t=25] Влажность,%
- * @returns
- */
-function dewpoint(t = 25, h = 50) {
-	const q = 17.27
-	const b = 237.7
-	const f = (q * t) / (b + t) + Math.log(h / 100)
-	return (b * f) / (q - f)
-}
+
+const dataBar = [
+	{ name: '20.02.2025', value: 100 },
+	{ name: '21.02.2025', value: 5 },
+	{ name: '22.02.2025', value: 15 },
+	{ name: '18.02.2025', value: 55 },
+	{ name: '19.02.2025', value: 55 },
+	{ name: '17.02.2025', value: 55 },
+	{ name: '16.02.2025', value: 55 },
+	{ name: '14.02.2025', value: 55 },
+	{ name: '15.02.2025', value: 55 },
+]
+
+const dataLine = [
+		{ side: 'left', year: 1956, miles: 3683.6965, gas: 2.3829 },
+		{ side: 'right', year: 1957, miles: 3722.7648, gas: 2.4026 },
+		{ side: 'bottom', year: 1958, miles: 3776.8595, gas: 2.2539 },
+		{ side: 'top', year: 1959, miles: 3912.0962, gas: 2.3079 },
+		{ side: 'right', year: 1960, miles: 3942.1488, gas: 2.2658 },
+		{ side: 'bottom', year: 1961, miles: 3984.2224, gas: 2.2526 },
+		{ side: 'right', year: 1962, miles: 4089.4064, gas: 2.2158 },
+		{ side: 'bottom', year: 1963, miles: 4230.6536, gas: 2.1237 },
+		{ side: 'bottom', year: 1964, miles: 4383.9219, gas: 2.1039 },
+		{ side: 'bottom', year: 1965, miles: 4546.2059, gas: 2.1368 },
+		{ side: 'top', year: 1966, miles: 4681.4425, gas: 2.1421 },
+		{ side: 'bottom', year: 1967, miles: 4837.716, gas: 2.1408 },
+		{ side: 'right', year: 1968, miles: 5048.0841, gas: 2.1263 },
+		{ side: 'right', year: 1969, miles: 5216.3787, gas: 2.0737 },
+		{ side: 'right', year: 1970, miles: 5384.6732, gas: 2.0118 },
+		{ side: 'bottom', year: 1971, miles: 5652.1412, gas: 1.9316 },
+		{ side: 'bottom', year: 1972, miles: 5979.7145, gas: 1.8737 },
+		{ side: 'right', year: 1973, miles: 6160.0301, gas: 1.9026 },
+		{ side: 'left', year: 1974, miles: 5946.6566, gas: 2.3447 },
+		{ side: 'bottom', year: 1975, miles: 6117.9564, gas: 2.3079 },
+	]

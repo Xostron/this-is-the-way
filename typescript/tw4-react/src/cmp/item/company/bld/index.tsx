48000d91f50f781eb58328757dfa0b804a0c4348
Xostron <aat9044107480@gmail.com>
2025-06-24 09:39:40 +0300

    fix

--- typescript/tw4-react/src/cmp/item/company/bld/index.tsx
+++ typescript/tw4-react/src/cmp/item/company/bld/index.tsx
@@ -6,10 +6,9 @@ interface IProps {
 
 export default function ItemBld(props: IProps) {
 	const { code, on, sectionCount, type, _id } = props.data
-	if (_id == '66c73ed6320b24af61331171') console.log(222, props.data)
 	return (
-		<article className='cmp-item-company-bld back-img-type-bld' data-img={type} title={_id}>
-			{/* <img src={`/img/type/${type}.svg`} alt='type' /> */}
+		<article className='cmp-item-company-bld' title={_id}>
+			<div className='cmp-item-company-bld-content back-img-type-bld' data-img={type}></div>
 			{code}
 		</article>
 	)

91fadb3cd022a991cfcf04c59ea761f86b2196f8
Xostron <aat9044107480@gmail.com>
2025-07-10 17:27:19 +0300

    fix css

--- typescript/tw4-react/src/cmp/item/company/bld/index.tsx
+++ typescript/tw4-react/src/cmp/item/company/bld/index.tsx
@@ -8,7 +8,11 @@ export default function ItemBld(props: IProps) {
 	const { code, on, sectionCount, type, _id } = props.data
 	return (
 		<article className='cmp-item-company-bld' title={_id}>
-			<div className='cmp-item-company-bld-content back-img-type-bld' data-img={type}></div>
+			<div
+				className='cmp-item-company-bld-content back-img-type-bld'
+				data-alarm={2}
+				data-img={type}
+			></div>
 			{code}
 		</article>
 	)

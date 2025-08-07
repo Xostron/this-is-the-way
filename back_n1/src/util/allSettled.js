567c275e1293707b42e4d126c736f4e7658eef34
Xostron <aat9044107480@gmail.com>
2025-08-08 00:01:38 +0300

    fix

--- back_n1/src/util/allSettled.js
+++ back_n1/src/util/allSettled.js
@@ -4,12 +4,24 @@ const p = [
 	fetch('https://jsonplaceholder.typicode.com/todos/1'),
 ]
 Promise.allSettled(p)
-	.then((r)=>{
-        // console.log(111, r)
-        r = r.map(el=>el.status=='fulfilled' ? el.value.json() : el)
-        return Promise.all(r)
-    })
-    .then(r=>{
-        console.log(r)
-    })
+	.then((r) => {
+		r = r.map((el) => (el.status == 'fulfilled' ? el.value.json() : el))
+		return Promise.all(r)
+	})
+	.then((r) => {
+		r.forEach
+		// console.log(r)
+	})
 	.catch((error) => console.log('Ошибка', error))
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then((r) => r.json())
+	.then((r) => {
+		console.log(r)
+        if (r.id===1) throw new Error('fucked up')
+		// throw new Error('Синхронная ошибка') // Не будет поймана
+	})
+	.catch((error) => {
+		console.log('Error 12', error.message, error)
+		// Этот catch не сработает!
+	})

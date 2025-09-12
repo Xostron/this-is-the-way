0922be3d02dbb0e1f126de6b8a68c56eef5e3d99
Xostron <aat9044107480@gmail.com>
2025-09-12 17:28:55 +0300

    fix

--- scraper/index.js
+++ scraper/index.js
@@ -13,41 +13,52 @@ const html = `
 <li alt='asds'></li>
 <img classname='' alt=''/>
 `
-const keyword = 'XOSTRON '
+const keyword = 'XOSTRON'
 
 function autoreplace(html, keyword) {
 	if (!keyword) return html
-	// const all = ['<title', '<h1']
-	const all = ['<title']
-	const partly = ['<p', '<span', '<li', 'alt']
-	let result
+	const all = ['<title', '<h1']
+	const partly = ['<p', '<span', '<li', `alt='`, `alt="`]
+	let result = html
 
-	all.forEach((el) => (result = paste(html, el, keyword)))
-	// partly.forEach((el) => (result = paste(html, el, keyword, true)))
+	all.forEach((el) => (result = paste(result, el, keyword)))
+	partly.forEach((el) => (result = paste(result, el, keyword, true)))
 	return result
 }
 
 function paste(html, tag, keyword, random = false) {
 	let cursor = html
-	let result
-	// Первое вхождение
-	const idxStart = cursor.indexOf(tag)
-	if (idxStart === -1) return result
+	let result = ''
+	console.log(555, keyword)
+	while (true) {
+		// Первое вхождение
+		const idxStart = cursor.indexOf(tag)
+		if (idxStart === -1) return result + cursor
 
-	result = cursor.slice(0, idxStart)
-	cursor = cursor.slice(idxStart, cursor.length)
-	console.log(1, result)
-	console.log(2, cursor)
-	// Завершающее вхождение
-	const idxEnd = cursor.indexOf('>')
-	const sub = cursor.slice(0, idxEnd+1)
-	console.log(3, sub)
-	cursor = cursor.slice(idxEnd+1, cursor.length)
-	console.log(4, cursor)
-	result += sub + keyword + cursor
-	console.log(5, result)
+		result += cursor.slice(0, idxStart)
+		cursor = cursor.slice(idxStart, cursor.length)
+		console.log(1, result)
+		console.log(2, cursor)
+		// Завершающее вхождение
+		const idxEnd =
+			tag !== `alt='` || tag !== `alt="`
+				? cursor.indexOf('>')
+				: tag === `alt="`
+				? cursor.indexOf(`"`)
+				: cursor.indexOf(`'`)
+		if (idxEnd === -1) {
+			result += cursor
+			continue
+		}
+		const sub = cursor.slice(0, idxEnd + 1)
+		console.log(3, sub)
+		cursor = cursor.slice(idxEnd + 1, cursor.length)
+		console.log(4, cursor)
+		result += sub + keyword
+		console.log(5, result)
 
-	return result
+		if (!cursor.length) return result + cursor
+	}
 }
 
 console.log(111, autoreplace(html, keyword))

eb470e4485c8c6f688b723d8b68571850b946806
Xostron <aat9044107480@gmail.com>
2025-12-30 10:53:09 +0300

    fix input

--- front-react/src/page/secret/index.jsx
+++ front-react/src/page/secret/index.jsx
@@ -1,11 +1,12 @@
+import Input from '@src/cmp/fields/input'
 
 const Secret = () => {
-  return (
-    <main className="page">
-      Secret
-    </main>
-  );
-};
+	return (
+		<main className='page'>
+			Secret
+			<Input cls='text' placeholder='Введите текст' title='Параметр' disabled={true}/>
+		</main>
+	)
+}
 
-export default Secret;
-
+export default Secret

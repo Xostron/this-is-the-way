8239b03a5efa6fc9a3ade23114244ffb69390c1c
Xostron <aat9044107480@gmail.com>
2026-02-01 21:10:57 +0300

    create empty .c

---
+++ project_C/Project2/Project2/test3.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Rus");
+	int _int1 = 12, _int2 = -42;
+	double _double = 12;
+	char _char = 'Ю';
+	_int1 = _int2;
+	_int2 = _int1;
+	printf("%s %d %d\n", "Переменные int", _int1, _int2);
+	printf("%s %f\n", "Переменные double", _double);
+	printf("%s %c\n", "Переменные char", _char);
+	return 0;
+}
+//pwduser:Ks9ex72F666HqweDgd

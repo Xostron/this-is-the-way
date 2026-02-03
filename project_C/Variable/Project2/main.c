60a33cfec71a996db17eae04645af57de6a9b581
Xostron <aat9044107480@gmail.com>
2026-02-04 01:04:23 +0300

    testing C

--- project_C/Variable/Project2/main.c
+++ project_C/Variable/Project2/main.c
@@ -5,12 +5,12 @@ int main(void) {
 	setlocale(LC_ALL, "Rus");
 	int _int1 = 12, _int2=-42;
 	double _double = 12;
-	char _char = 'Ю';
+	char _char = 'd';
 	_int1 = _int2;
 	_int2 = _int1;
-	printf("%s %d %d\n", "Переменные int", _int1, _int2);
-	printf("%s %f\n", "Переменные double",  _double);
-	printf("%s %c\n", "Переменные char", _char);
+	printf("%s %d %d\n", "РџРµСЂРµРјРµРЅРЅС‹Рµ int", _int1, _int2);
+	printf("%s %f\n", "РџРµСЂРµРјРµРЅРЅС‹Рµ double",  _double);
+	printf("%s %c\n", "РџРµСЂРµРјРµРЅРЅС‹Рµ char", _char);
 	return 0;
 }
 //pwduser:Ks9ex72F666HqweDgd

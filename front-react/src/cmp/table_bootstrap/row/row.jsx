a3e6a045012781f5e570467a11f649ef13d5c6b6
Xostron <aat9044107480@gmail.com>
2026-03-04 00:00:08 +0300

    fix

--- front-react/src/cmp/table_bootstrap/row/row.jsx
+++ front-react/src/cmp/table_bootstrap/row/row.jsx
@@ -17,7 +17,6 @@ function Row({ row, orderRow }) {
 				const FCell = defCells?.[el.type] ?? <Cell key={el.id} obj={{ value: 'Неопределен тип ячейки' }} />
 				// Мясо ячейки
 				const obj = el.type == 'payment' ? row.payment.find((t) => t.order === el.monthId) : row
-				console.log(el, row, el.getValue(obj, el, orderRow))
 				return <FCell key={el.id} obj={el.getValue(obj, el, orderRow)} />
 			})}
 		</tr>

6227a8005f9aff9c3335c43c03b88595309ef8b3
Xostron <aat9044107480@gmail.com>
2026-05-28 16:01:28 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/catalog--odezhda-obuv-i-aksessuary/54432/list?tabId=male_tab&generalContext=&hid=7811881&rs=eJwz8njByPiJ0YmDUWDlszvMEkwaT17cYdZ4BiLOgojmk6waC5ZYaLwAcZ6DiF_bpDTOgxiPQcSLO9PZNWbuns4OAL1HIo8%2C',
+	'https://market.yandex.ru/',
 ]
 
 main()

6a4bb5fa81acdb39294b56bf7ece03e34bb6dfd1
Xostron <aat9044107480@gmail.com>
2026-05-28 11:34:46 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/catalog--tovary-dlia-avto-i-mototekhniki/54418/list?generalContext=&hid=90402&rs=eJwz8v_E6MPBILDwEKsEg8KiQ6wai4F4DxAfBuLrQHwXiN8CceNhVo0uIJ78rJ1VYwWI2AQiWq8sZdM4e5pV4_MZVo3n3TwA2kUgaA%2C%2C&super-price-filter=1',
+	'https://market.yandex.ru/catalog--odezhda-obuv-i-aksessuary/54432/list?tabId=male_tab&generalContext=&hid=7811881&rs=eJwz8njByPiJ0YmDUWDlszvMEkwaT17cYdZ4BiLOgojmk6waC5ZYaLwAcZ6DiF_bpDTOgxiPQcSLO9PZNWbuns4OAL1HIo8%2C',
 ]
 
 main()

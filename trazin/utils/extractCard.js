9c5c254524590e29773e05d1393ba84135d2b5df
Xostron <aat9044107480@gmail.com>
2026-05-29 00:42:14 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/',
+	'https://market.yandex.ru/catalog--produkty/54434/list?hid=91325&rs=eJwzCvvEGMzBILD3JKsEk8Lqk6wah4D48cmb7Bpv9uxn17gFIo4DhR6CGH8mAYknkxrZNR5232LXeAASewcSewFiPe9Yx6ax7BSrxv-TrACAoidx',
 ]
 
 main()
--- trazin/utils/getPromo.js
+++ trazin/utils/getPromo.js
@@ -3,7 +3,7 @@ const fnConfig = require('../scrapper/config')
 const save = require('../scrapper/save')
 const { delay } = require('../tool/index')
 const { fnPromo } = require('../scrapper')
-const data = require('../temp_cards/com_1.json')
+const data = require('../temp_cards/bakaley.json')
 // ссылки на товары
 
 main()

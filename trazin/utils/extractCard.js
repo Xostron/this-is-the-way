55af502b80722375bfcf4740786bcd059ada8e9f
xostron <xostron8@gmail.com>
2026-05-30 02:59:06 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/search?text=%D1%81%D1%83%D0%BC%D0%BA%D0%B8%20%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B5&hid=7812201&hid=91076&rs=eJwzSqti5Xj54g7zC0bGT4y8HAwSDAogrsKR46x_GcVkXu5rYuLieH69nV1gQ_MFXjjn0Kd2HjjnFZCAc14CiV6mlLW89lOZ2sw47VcwgcwDADu-KM4%2C&rt=9&glfilter=14805991%3A14805993',
+	'https://market.yandex.ru/search?text=%D0%BE%D0%B1%D1%83%D0%B2%D1%8C%20%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F&hid=53253309&nid=53253313&rs=eJwzct7AyPiCkfEToxUHg8DCQ6wSDAp7V26S1PgAIhavAhLnVwOJdhBrMoi1BsR6CyL2gIjDQDEAfOgcQw%2C%2C&rt=10&glfilter=14805991%3A14805993',
 ]
 
 main()
--- trazin/utils/getPromo.js
+++ trazin/utils/getPromo.js
@@ -3,7 +3,7 @@ const fnConfig = require('../scrapper/config')
 const save = require('../scrapper/save')
 const { delay } = require('../tool/index')
 const { fnPromo } = require('../scrapper')
-const data = require('../temp_cards/bag_3.json')
+const data = require('../temp_cards/shoes_2.json')
 // ссылки на товары
 
 main()

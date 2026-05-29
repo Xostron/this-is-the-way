e95eccabaed91be6996c40e98721ea883bee8896
Xostron <aat9044107480@gmail.com>
2026-05-29 14:11:19 +0300

    fix

--- trazin/utils/getPromo.js
+++ trazin/utils/getPromo.js
@@ -3,7 +3,7 @@ const fnConfig = require('../scrapper/config')
 const save = require('../scrapper/save')
 const { delay } = require('../tool/index')
 const { fnPromo } = require('../scrapper')
-const data = require('../temp_cards/autoclean.json')
+const data = require('../temp_cards/home_tech.json')
 // ссылки на товары
 
 main()

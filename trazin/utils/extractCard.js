8e86e9f349e14129cab5bca3b07149cdad0fdbbb
Xostron <aat9044107480@gmail.com>
2026-05-30 21:38:17 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/catalog--detskaia-odezhda-i-obuv-v-volzhskom/58898/list?rs=eJwzUnrByPiJUYaDUWDhIVYJBo0LCyZKatz7-UdCY-NMIGvlszvMANxzDe4%2C&super-price-filter=1',
+	'https://market.yandex.ru/page/crossborder_dep?generalContext=t%3DcprPage%3Bcpk%3Dcrossborder_dep%3B&rs=eJwz4nrByPiJkYWDQYIBABL7Aj4%2C',
 ]
 
 main()

a9ef5c881d51eb218bb1b8c64c400cdcb599e6f7
xostron <xostron8@gmail.com>
2026-05-30 22:59:35 +0300

    fix

--- trazin/utils/getPromo.js
+++ trazin/utils/getPromo.js
@@ -3,7 +3,7 @@ const fnConfig = require('../scrapper/config')
 const save = require('../scrapper/save')
 const { delay } = require('../tool/index')
 const { fnPromo } = require('../scrapper')
-const data = require('../temp_cards/sales.json')
+const data = require('../temp_cards/world.json')
 // ссылки на товары
 
 main()

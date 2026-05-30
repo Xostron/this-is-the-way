49973c5cd3c78191ad3a6a7e7c3cf740cb868ef3
xostron <xostron8@gmail.com>
2026-05-30 23:24:08 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/page/crossborder_dep?generalContext=t%3DcprPage%3Bcpk%3Dcrossborder_dep%3B&rs=eJwz4nrByPiJkYWDQYIBABL7Aj4%2C',
+	'https://market.yandex.ru/catalog--tovary-dlia-stroitelstva-i-remonta-v-volzhskom/54503/list?generalContext=&hid=91597&rs=eJwzsv3EaMXBILDwEKsEg8LZ06waN4F44xlWjR1APO9FksZiIL7z9AOrxtEFd9k1Fh1i1Vh1hFXjM1D2WTcPAAqLGk8%2C',
 ]
 
 main()

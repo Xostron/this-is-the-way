85b50d9fbd868bc9e0a9796850228c8cd357bd44
xostron <xostron8@gmail.com>
2026-06-03 23:29:00 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,6 +3,8 @@ const { fnUrlCards } = require('../scrapper')
 // Ссылка на каталог
 const data = [
 	'https://market.yandex.ru/business--acmer-store/216468891?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D216468891%3B&rs=eJwzEv_EKMLBKLDwEKsEg0bPUVaNz2dYNc6eZgUAWgUIGQ%2C%2C&searchContext=sins_ctx',
+'https://market.yandex.ru/business--traktornyi-zavod-rusich/176568334?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D176568334%3B&rs=eJwzEv_EKMLBKLDwEKsEg8aiQ6wa94-wapw9zQoAWh0IEA%2C%2C&searchContext=sins_ctx',
+'',
 ]
 
 main()

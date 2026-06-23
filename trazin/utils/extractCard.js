8a68abcafe86a33da99b7ae27a87e2ffc8d65749
Xostron <aat9044107480@gmail.com>
2026-06-23 13:26:30 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require("../scrapper");
 
 // Ссылка на каталог
 const data = [
-  "https://market.yandex.ru/?auth_renewed=1",
+  "https://market.yandex.ru/business--rival/890754?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D890754%3B&rs=eJwzEv7EKMDBKLDwEKsEg8aiQ6wa94-wAgA7uwZD&searchContext=sins_ctx",
 ];
 
 main();
@@ -11,6 +11,6 @@ main();
 async function main() {
   const arr = fnConfig(data, null, true);
   for (const el of arr) {
-    await fnUrlCards(el, 5);
+    await fnUrlCards(el, 2);
   }
 }

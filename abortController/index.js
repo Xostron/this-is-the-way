8277e9f0e26e3feca1d15cbcc6ab37cca2bc1c5a
Xostron <aat9044107480@gmail.com>
2026-06-26 17:22:52 +0300

    fix

--- abortController/index.js
+++ abortController/index.js
@@ -1,1 +1,17 @@
 console.log('CUSTODES')
+
+let sum1 = 0
+
+// 42 занятий 42часов
+new Array(42).fill(1).forEach((el, i) => {
+	sum1 += 800
+})
+
+console.log('Волская автошкола', sum1)
+
+// 28 занятий по 1,5ч = 42ч
+sum1 = 0
+new Array(28).fill(1).forEach((el, i) => {
+	sum1 += 1060
+})
+console.log('Sprint автошкола', sum1)
--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,9 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
-const data = [
-	'https://market.yandex.ru/search?text=%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B8%20%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%B8%D0%B5&hid=53546007&hid=53546173&how=rating&rs=eJwzOsyoVMAlf7HlYvPFpgsbL-y7sPvCrgs7FC7sudh8YdvFRhDnwlaB59fb2ZVYODgFBIAkgwADhNRgyCKks4rD0MTCwNTS0qiBcX37EckuRiYOhipWjukzjkhuYGR4wcj4iVEGaNbCQ6wSDAogUYW9M49IaoBYGiDWX0YGIGhi4uIAuUHgBZDoZQIJTQWTK5hAKgEww1EN&glfilter=14805991%3A14805992',
-]
+const data = ['https://market.yandex.ru/']
 
 main()
 

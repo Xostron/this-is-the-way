f06054d965f485c7c46fa8442b2a347797eb3018
Xostron <aat9044107480@gmail.com>
2026-06-29 17:07:19 +0300

    fix trazin

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,7 +2,7 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
-const data = ['https://market.yandex.ru/']
+const data = ['https://market.yandex.ru/catalog--naushniki-i-bluetooth-garnitury-v-volzhskom/26992150/list?hid=90555&how=rating&rs=eJwz4v7EyMHBIMGgsPswKwAS3wL8']
 
 main()
 

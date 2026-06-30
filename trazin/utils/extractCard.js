292f207a263068b00da9470977b604c167635f26
Xostron <aat9044107480@gmail.com>
2026-06-30 17:01:13 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,13 +2,13 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
-const data = ['https://market.yandex.ru/catalog--planshety-v-volzhskom/26908970/list?hid=6427100&how=rating&rs=eJwz4vnEyMnBIMGgcGdxBzMAFx8DhQ%2C%2C']
+const data = ['https://market.yandex.ru/catalog--tovary-dlia-stroitelstva-i-remonta-v-volzhskom/54503/list?generalContext=&hid=91597&how=rating&rs=eJwzsv7EaMHBILDwEKsEg8LZ06waN4F44xlWjR1APO9FksZiIL7z9AOrxtEFd9k1zh1m1Xj8fxGTxs5JP5kA3KoaaA%2C%2C']
 
 main()
 
 async function main() {
 	const arr = fnConfig(data, null, true)
 	for (const el of arr) {
-		await fnUrlCards(el, 4)
+		await fnUrlCards(el, 5)
 	}
 }

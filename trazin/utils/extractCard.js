60d67533bb7e497a6728a6bedc289d904a7a3d69
xostron <xostron8@gmail.com>
2026-07-02 23:11:19 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,13 +2,15 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
-const data = ['https://market.yandex.ru/page/weekly?tabId=all_tab&utm_medium=sharing&generalContext=t%3DcprPage%3Bcpk%3Dweekly%3B&rs=eJwz4nrByPiJkYWDQYIBABL7Aj4%2C']
+const data = ['https://market.yandex.ru/search?text=%D0%BE%D1%82%D0%B2%D0%B5%D1%80%D1%82%D0%BA%D0%B8&hid=91630&hid=15721749&hid=278423&hid=15221832&rs=eJwz8qli4Xh3mvUTow4Hg8DCQ6wSDApArsLUU_vZFab_F1A40bGQXQMoogES0QCKaIBE_jIyAEEvE4icCiZXMAEVAQBPMBoo&rt=9',
+	'https://market.yandex.ru/business--marshall/895958?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D895958%3B&rs=eJwzEv_EKMLBKLDwEKsEg8aiQ6wan8-wapw9zQoAWtkILA%2C%2C&searchContext=sins_ctx'
+]
 
 main()
 
 async function main() {
 	const arr = fnConfig(data, null, true)
 	for (const el of arr) {
-		await fnUrlCards(el, 4.5)
+		await fnUrlCards(el, 2.5)
 	}
 }
--- trazin/utils/getPromo.js
+++ trazin/utils/getPromo.js
@@ -5,8 +5,8 @@ const save = require('../tool/save')
 const { delay } = require('../tool/time')
 const data = [
 //   require('../temp_cards/0.json'),
-require('../temp_cards/cards_0.json'),
-// require('../temp_cards/cards_1.json'),
+// require('../temp_cards/cards_0.json'),
+require('../temp_cards/cards_1.json'),
     //   require('../temp_cards/cards_2.json'),
     //   require('../temp_cards/cards_3.json'),
     //   require('../temp_cards/cards_4.json'),

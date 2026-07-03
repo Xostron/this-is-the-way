98109d43caee3057e8d3c1926a89cb35dca1ec87
Xostron <aat9044107480@gmail.com>
2026-07-03 16:26:08 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -2,8 +2,10 @@ const fnConfig = require('../scrapper/config')
 const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
-const data = ['https://market.yandex.ru/search?text=%D0%BE%D1%82%D0%B2%D0%B5%D1%80%D1%82%D0%BA%D0%B8&hid=91630&hid=15721749&hid=278423&hid=15221832&rs=eJwz8qli4Xh3mvUTow4Hg8DCQ6wSDApArsLUU_vZFab_F1A40bGQXQMoogES0QCKaIBE_jIyAEEvE4icCiZXMAEVAQBPMBoo&rt=9',
-	'https://market.yandex.ru/business--marshall/895958?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D895958%3B&rs=eJwzEv_EKMLBKLDwEKsEg8aiQ6wan8-wapw9zQoAWtkILA%2C%2C&searchContext=sins_ctx'
+const data = [
+	// 'https://market.yandex.ru/search?text=%D1%80%D1%83%D0%B1%D0%B0%D1%88%D0%BA%D0%B8&hid=53546043&hid=52710858&hid=53546173&rs=eJwz8q5i5dg944jkC0bGT4xqHAwCCw-xSjAogIQUTs2eKKmwd-YRSQ0QVwPE1QBx_zIyAEEvE4icCiZXMIFUAAAccxmK&rt=9',
+	'https://market.yandex.ru/catalog--ruchnoi-stroitelnyi-instrument-v-volzhskom/56346/list?hid=91625&rs=eJwzmsf4iXEWIweDwMJDrBIMCi9Ps2osbdvApPFm52V2jZ99H9g0fs16wa4x9dR-do25_wU0zrev4NB4DVT1aPNVdo3pQJF3QM4HIH5-YjmHxuLlhzk0XgF5t37vZdf48PgDq8ayU2fYNGbuB-lvX8Gu8RYk9uvjEXaNN0BlsxZeY9U4vv8Cm8aON9dZNe5tAZr-7-VXdo0vIPsPHmcFANldTCc%2C',
+	// 'https://market.yandex.ru/category/prinadlezhnosti-dlya-moyki-avtomobiley',
 ]
 
 main()
@@ -11,6 +13,6 @@ main()
 async function main() {
 	const arr = fnConfig(data, null, true)
 	for (const el of arr) {
-		await fnUrlCards(el, 2.5)
+		await fnUrlCards(el, 5)
 	}
 }

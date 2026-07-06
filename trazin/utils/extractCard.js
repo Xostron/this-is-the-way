f349001a7be000f695b6e92be91d7d47f132f6c0
xostron <xostron8@gmail.com>
2026-07-07 01:54:10 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -1,17 +1,16 @@
-const fnConfig = require('../scrapper/config')
-const { fnUrlCards } = require('../scrapper')
+const fnConfig = require("../scrapper/config");
+const { fnUrlCards } = require("../scrapper");
 
 // Ссылка на каталог
 const data = [
-	// 'https://market.yandex.ru/',
-	'https://market.yandex.ru/category/prinadlezhnosti-dlya-moyki-avtomobiley',
-]
+    "https://market.yandex.ru/search?text=warhammer%2040000&hid=10682647&hid=10683227&hid=278335&hid=10683226&hid=10470548&hid=278342&hid=14960839&hid=18540110&hid=12494574&hid=53546007&rs=eJwzqqhi5Zje1MP6iTGMg0Fg4SFWCQYFEF_hdhuQ2P9PQOEWiDGl8z-LwjEg7_jEiewK58685lB4d_YXq8L0GUckNS5OjteYf5RV49YRVo0_QHwEiEFKNNa3H5H8y8gABL1MIHIqmFzBBLIAAHecLd8%2C&rt=9",
+];
 
-main()
+main();
 
 async function main() {
-	const arr = fnConfig(data, null, true)
-	for (const el of arr) {
-		await fnUrlCards(el, 5)
-	}
+    const arr = fnConfig(data, null, true);
+    for (const el of arr) {
+        await fnUrlCards(el, 3);
+    }
 }

91e5fe7209f6b90ce703be3e9d287a8dee873c34
xostron <xostron8@gmail.com>
2026-07-16 00:48:23 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require("../scrapper");
 
 // Ссылка на каталог
 const data = [
-    "https://market.yandex.ru/search?text=%D0%BF%D0%B0%D0%BA%D0%B5%D1%82%D1%8B&hid=5013720&hid=12501724&hid=6943877&hid=12807791&how=rating&rs=eJwzCqpi5bjRuInpE6MBB4PAwkOsEgwKIL7CndbfrAqtL5czK7y_08umAeJqgCQ0QGIaILG_jAxA0MsEIqeCyRVMIBUANO4dlw%2C%2C&rt=9"
+    "https://market.yandex.ru/catalog--posuda-i-kukhonnye-prinadlezhnosti-v-volzhskom/54498/list?hid=90692&how=rating&rs=eJwzMvjEqMvBILDwEKsEg8KRI6wa-1p_s2o82xevsfJeM5vGgi37mTUWgoizQLklQAYA0ZkUEw%2C%2C"
 ];
 
 main();

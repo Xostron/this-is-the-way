e8a327c062b226c2adf8451b228d0b561e53d6b5
Xostron <aat9044107480@gmail.com>
2026-07-17 17:16:46 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require("../scrapper");
 
 // Ссылка на каталог
 const data = [
-    "https://market.yandex.ru/search?generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D850885%3B&text=%D0%9C%D1%8F%D1%81%D0%BE%D1%80%D1%83%D0%B1%D0%BA%D0%B8&rs=eJwzUvrEKM_BKLDwEKsEg8adw6waL4G4ofuMokbTk3OKGrfvn1YEANrdDhA%2C&businessId=850885&searchContext=sins_ctx&merchant-filter=850885"
+    "https://market.yandex.ru/catalog--elektronika/54440/list?generalContext=&hid=198119&rs=eJwBSwC0_zJJ8gFGCAAQocIFGAAg54sMKO_DBSjFygUo7M85KMnjPCjRk18o4N6vAijB-4YFKKLCBSiqxAUojMUFKLHFBSiBxwUozcsFKOaLDGhQHs8%2C"
 ];
 
 main();
@@ -11,6 +11,6 @@ main();
 async function main() {
     const arr = fnConfig(data, null, true);
     for (const el of arr) {
-        await fnUrlCards(el, 3);
+        await fnUrlCards(el, 5);
     }
 }

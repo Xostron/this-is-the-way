4cf00125484374f4758f4fb29db824b1ec62707c
Xostron <aat9044107480@gmail.com>
2026-07-27 17:23:07 +0300

    fix

--- go/2026/GO_bootcamp_01/task_2/main.go
+++ go/2026/GO_bootcamp_01/task_2/main.go
@@ -1,11 +1,34 @@
-package task2
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	// "strconv"
 )
 
-func main() {
-	fmt.Println("hi")
+type WordStat struct {
+	Word  string
+	Count int
+}
 
+func main() {
+	// Читаем строку
+	str := bufio.NewScanner(os.Stdin)
+	str.Scan()
+	line := str.Text()
+
+	// Разбиваем строку на слова
+	words := strings.Fields(line)
+
+	// Создание мапы
+	counts := make(map[string]int)
+	for v, word := range words {
+		fmt.Println(11, v, word)
+		counts[word]++
+	}
+	fmt.Println("input", line, "words", words)
+	fmt.Println("counts", counts)
+	// return []string{}
 }

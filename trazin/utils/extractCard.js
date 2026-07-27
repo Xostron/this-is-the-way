9d042acc1bb3688e91972fbc6f157f1660e7414e
Xostron <aat9044107480@gmail.com>
2026-07-27 16:21:41 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require("../scrapper");
 
 // Ссылка на каталог
 const data = [
-    "https://market.yandex.ru/search?text=%D0%BE%D1%87%D0%BA%D0%B8&hid=433018&hid=15625429&hid=90534&hid=10972670&hid=15720387&hid=6527202&rs=eJwzSqpi4fi1TeoFI-MnRnsOBoGFh1glGBSAIgpXb-5kV1h2mFXh3-25rAqH9-9nV3i0sY9ZAyincRAop_HyNKsGUF4DJK8BkvrLyAAEvUwgciqYXMEEVA0Awj8lWA%2C%2C&rt=9"
+    "https://market.yandex.ru/search?text=%D0%BA%D0%B0%D0%BC%D0%B5%D1%80%D0%B0%20%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%D0%BD%D0%B0%D0%B1%D0%BB%D1%8E%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F&hid=6202209&hid=15450276&hid=6206018&how=rating&rs=eJwzcqpi5Xh47BfTJ0YFDgaBhYdYJRgUQHyFJY3r2RUOPfnFpDHnLKsGiPeXkQEIeplA5FQwuYIJpBQASOAW1A%2C%2C&rt=9"
 ];
 
 main();

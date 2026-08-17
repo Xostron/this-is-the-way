13c1e9ba4586ea7240bbc0b99858d37bb945879a
Xostron <aat9044107480@gmail.com>
2026-08-17 16:24:43 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -1,16 +1,16 @@
-const fnConfig = require("../scrapper/config");
-const { fnUrlCards } = require("../scrapper");
+const fnConfig = require('../scrapper/config')
+const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
 const data = [
-    "https://market.yandex.ru/search?text=%D0%BA%D0%B0%D0%BC%D0%B5%D1%80%D0%B0%20%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%D0%BD%D0%B0%D0%B1%D0%BB%D1%8E%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F&hid=6202209&hid=15450276&hid=6206018&how=rating&rs=eJwzcqpi5Xh47BfTJ0YFDgaBhYdYJRgUQHyFJY3r2RUOPfnFpDHnLKsGiPeXkQEIeplA5FQwuYIJpBQASOAW1A%2C%2C&rt=9"
-];
+	'https://market.yandex.ru/search?text=%D0%BF%D0%BE%D0%BA%D1%80%D1%8B%D1%88%D0%BA%D0%B8%20%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5&hid=90490&rs=eJwz0qhi4fh1iPUTIwcHgwSDApD5l5EBCHqZQORUMLmCCSgMAOduCi0%2C&rt=9',
+]
 
-main();
+main()
 
 async function main() {
-    const arr = fnConfig(data, null, true);
-    for (const el of arr) {
-        await fnUrlCards(el, 4);
-    }
+	const arr = fnConfig(data, null, true)
+	for (const el of arr) {
+		await fnUrlCards(el, 4.4)
+	}
 }

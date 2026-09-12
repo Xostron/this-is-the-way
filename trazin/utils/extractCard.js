69b5d882544ce2449c6a0677d31f3b14bba3c30d
xostron <xostron8@gmail.com>
2026-09-13 01:28:13 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/search?text=%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B8&hid=53546007&hid=52710834&rs=eJwzcqxi5Zg-44jkC0bGT4wyHAwCCw-xSjAogIQUNs2eKKkBYmmAWH8ZGYCglwlETgWTK5hAkgDUURQI&rt=9',
+	'https://market.yandex.ru/search?text=%D0%B0%D1%8D%D1%80%D0%BE%D0%B3%D1%80%D0%B8%D0%BB%D1%8C&hid=987827&rs=eJwzSlFy5hK6sOFi78WGC_subAaSOy7svtgjcOtML5cSCweDACeYZICQGgxZDFUcxiYGZoamxkYNjJuX2nQxMnEwVrFwAJkbGBk-MXJwMEgwKAB5K5iAxF6m0kW_bQFCdCFm&rt=11&glfilter=21194330%3A34061532&is_formalized=1',
 ]
 
 main()

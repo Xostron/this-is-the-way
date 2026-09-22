f2f4babcc8cacbc02476e031ae8168f7e8573b6a
Xostron <aat9044107480@gmail.com>
2026-09-22 16:34:20 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/search?text=%D0%BF%D1%8B%D0%BB%D0%B5%D1%81%D0%BE%D1%81&hid=16302537&hid=16302535&hid=16302536&hid=281934&rs=eJwzsqpi5TjZ_Jj9E6MKB4PAwkOsEgwKIL7CcRBxAkScmyWo8XbBDXYNIGMFE0hyL9OM74V2AKnpGIM%2C&rt=9',
+	'https://market.yandex.ru/catalog--detskie-tovary/54421/list?generalContext=&hid=10852916&rs=eJwziv7EGMTBILBlyzRWCVaFnqOsGjd3_2fRWNPUw6px8uM2No13H4DE00Yg9wGI-PGmnV1j__6vrBrLG601TnffZ9f4ASK-fQUq-wVS8QFI7GVyvLnKHgA3Uylp',
 ]
 
 main()

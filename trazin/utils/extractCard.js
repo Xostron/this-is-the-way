f776cce19b1e859633f81a2ed9a13a72a4156364
Xostron <aat9044107480@gmail.com>
2026-09-25 17:29:38 +0300

    fix

--- trazin/utils/extractCard.js
+++ trazin/utils/extractCard.js
@@ -3,7 +3,7 @@ const { fnUrlCards } = require('../scrapper')
 
 // Ссылка на каталог
 const data = [
-	'https://market.yandex.ru/catalog--tovary-dlia-avto-i-mototekhniki/54418/list?generalContext=&hid=90402&rs=eJwz8vnE6MzBILDwEKsEg8KiQ6wai4F4DxAfBuLrQHwXiN8CceNhVo3Jz9pZNVaAiNYrS9k0zp5m1fh8hlXjeTfPXiaDuqf2AH2GHyc%2C',
+	'https://market.yandex.ru/search?text=%D0%B7%D0%B0%D1%80%D1%8F%D0%B4%D0%BD%D0%BE%D0%B5%20%D1%83%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE%20%D0%B4%D0%BB%D1%8F%20%D0%B0%D0%BA%D0%BA%D1%83%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%D0%B0%20%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8F&hid=6856242&hid=14946301&rs=eJwzMqpi5di0ZxHzJ0YZDgaBhYdYJRgUQHyFv_MnsGuAWBog1gomEHMv06GHy-wA6cYUxQ%2C%2C&rt=9',
 ]
 
 main()
